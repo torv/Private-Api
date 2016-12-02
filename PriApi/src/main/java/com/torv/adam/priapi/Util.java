c95b60ef8273dd61bda34791b32f9a8201f950ed
torv <aracclove@gmail.com>
2016-12-02 20:54:01 +0800

    remove useless header

--- PriApi/src/main/java/com/torv/adam/priapi/Util.java
+++ PriApi/src/main/java/com/torv/adam/priapi/Util.java
@@ -14,12 +14,12 @@ public class Util {
             headers = new HashMap<>();
         }
 
-        headers.put("Connection", "close");
-        headers.put("Accept", "*/*");
-        headers.put("X-IG-Capabilities", Constants.X_IG_Capabilities);
-        headers.put("X-IG-Connection-Type", "WIFI");
-        headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-        headers.put("Accept-Language", "en-US");
+//        headers.put("Connection", "close");
+//        headers.put("Accept", "*/*");
+//        headers.put("X-IG-Capabilities", Constants.X_IG_Capabilities);
+//        headers.put("X-IG-Connection-Type", "WIFI");
+//        headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+//        headers.put("Accept-Language", "en-US");
         String userAgent = "Instagram " + Constants.VERSION +" Android (23/6.0.1; 640dpi; 1440x2560; samsung; SM-G9200; zerofltechn; samsungexynos7420; zh_CN)";
         headers.put("User-Agent", userAgent);
 
